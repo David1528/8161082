b7fcfe1b6f27d356e07796b078860357c2be7f68
David David <davidka897@mail.ru>
2018-10-28 00:19:00 +0300

    DAO KPI department has been created

---
+++ local/modules/miet.kpi/lang/en/lib/kpidepartment.php
@@ -0,0 +1,6 @@
+<?
+$MESS["KPI_ENTITY_ID_FIELD"] = "ID KPI";
+$MESS["KPI_ENTITY_UF_VALUE_FIELD"] = "Value of the indicator";
+$MESS["KPI_ENTITY_UF_CREATED_FIELD"] = "The date the indicator value was entered";
+$MESS["KPI_ENTITY_UF_CHANGED_FIELD"] = "Date of last change";
+?>
